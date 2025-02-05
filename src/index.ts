d3b6ceb0db59a782c90659311d4144791bfbb908
Lalit <kumarlalitamohanty@gmail.com>
2025-02-05 16:41:25 +0530

    cors update

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ import allRoutes from "./routes/z.index.allroutes";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ origin: "*", methods: "GET,POST,PUT,DELETE,OPTIONS" }));
+
 
 app.get("/health", async (req: Request,res:Response) => {
    res.send({
